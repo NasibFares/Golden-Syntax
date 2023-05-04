52d9d8eedbf82ffcb1b11687e31c4b2d675d4e0a
nasibfares <nasib.y.fares@gmail.com>
2023-05-04 14:38:02 +0200

    Ein neues Projekt 'Golden Syntax' wurde erstellt.

--- GoldenSyntax/src/produkte/products/electronics/TVs.kt
+++ GoldenSyntax/src/produkte/products/electronics/TVs.kt
@@ -1,4 +1,11 @@
 package produkte.products.electronics
 
-class TVs {
+import produkte.Categories
+import produkte.Subcategories
+import produkte.products.Products
+
+class TVs(name:String,price: Double, evaluation: Double=0.0,
+          category: Categories,
+          subCategory: Subcategories,val size:Double,val art:List<String> = listOf("HD","UHD","4K")
+):Products(name,price,evaluation,category,subCategory) {
 }
