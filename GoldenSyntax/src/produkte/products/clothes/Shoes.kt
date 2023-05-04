61f290f355b7683df088fe314979b8aac2761b14
nasibfares <nasib.y.fares@gmail.com>
2023-05-04 14:22:43 +0200

    Ein neues Projekt 'Golden Syntax' wurde erstellt.

--- GoldenSyntax/src/produkte/products/clothes/Shoes.kt
+++ GoldenSyntax/src/produkte/products/clothes/Shoes.kt
@@ -1,4 +1,12 @@
 package produkte.products.clothes
 
-class Shoes {
+import produkte.Categories
+import produkte.Subcategories
+import produkte.products.Products
+
+class Shoes(
+    name: String, price: Double, evaluation: Double = 0.0,
+    category: Categories,
+    subCategory: Subcategories, val size: Double, val art: List<String> = listOf("Sport", "Summer", "Winter")
+) : Products(name, price, evaluation, category, subCategory) {
 }
--- GoldenSyntax/src/produkte/products/clothes/Trousers.kt
+++ GoldenSyntax/src/produkte/products/clothes/Trousers.kt
@@ -1,4 +1,12 @@
 package produkte.products.clothes
 
-class Trousers {
+import produkte.Categories
+import produkte.Subcategories
+import produkte.products.Products
+
+class Trousers(
+    name: String, price: Double, evaluation: Double = 0.0, category: Categories,
+    subCategory: Subcategories, val size: Int
+) : Products(name, price, evaluation, category, subCategory) {
+
 }
