c4c70f0d36eb422868db7223df4d562293496bc4
nasibfares <nasib.y.fares@gmail.com>
2023-05-04 14:40:50 +0200

    Ein neues Projekt 'Golden Syntax' wurde erstellt.

--- GoldenSyntax/src/produkte/products/electronics/Microwaves.kt
+++ GoldenSyntax/src/produkte/products/electronics/Microwaves.kt
@@ -1,4 +1,12 @@
 package produkte.products.electronics
 
-class Microwaves {
+import produkte.Categories
+import produkte.Subcategories
+import produkte.products.Products
+
+class Microwaves(
+    name: String, price: Double, evaluation: Double = 0.0,
+    category: Categories,
+    subCategory: Subcategories, val capacity: String, val colour: String, val multfunction: Boolean
+) : Products(name, price, evaluation, category, subCategory) {
 }
--- GoldenSyntax/src/produkte/products/electronics/TVs.kt
+++ GoldenSyntax/src/produkte/products/electronics/TVs.kt
@@ -4,8 +4,9 @@ import produkte.Categories
 import produkte.Subcategories
 import produkte.products.Products
 
-class TVs(name:String,price: Double, evaluation: Double=0.0,
-          category: Categories,
-          subCategory: Subcategories,val size:Double,val art:List<String> = listOf("HD","UHD","4K")
-):Products(name,price,evaluation,category,subCategory) {
+class TVs(
+    name: String, price: Double, evaluation: Double = 0.0,
+    category: Categories,
+    subCategory: Subcategories, val size: Double, val display: List<String> = listOf("HD", "UHD", "4K")
+) : Products(name, price, evaluation, category, subCategory) {
 }
