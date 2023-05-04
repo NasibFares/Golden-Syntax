f1eabb46efdec374a1b9c04f090028d0c777e29a
nasibfares <nasib.y.fares@gmail.com>
2023-05-04 15:00:01 +0200

    Ein neues Projekt 'Golden Syntax' wurde erstellt.

--- GoldenSyntax/src/benutzer/Customers.kt
+++ GoldenSyntax/src/benutzer/Customers.kt
@@ -1,8 +1,15 @@
 package benutzer
 import produkte.products.Products
+/*
+Die Klasse 'Customers'→ "Die Kunden des Shops" hat die Eigenschaften 'name'→ Name der Kunde, 'age'→ Alter der Kunde,
+paymentMethod→ Zahlungsmethode, 'cart'→ Warenkorb der Kunde, 'accountBalance'→ Kontostand der Kunde
+ */
 class Customers(val name: String, val age: Int, val paymentMethod: String, var cart: MutableList<Products> = mutableListOf()) {
     var accountBalance: Double = 0.0
-
+/*
+Die Methode 'addToCart' ermöglicht dem Kunden, die lieblingsprodukte in Warenkorb hinzufügen.
+Als Parameter übernimmt die Methode 'product→'
+ */
     fun addToCart(product: Products) {
         cart.add(product)
     }
@@ -12,6 +19,6 @@ class Customers(val name: String, val age: Int, val paymentMethod: String, var c
     }
 
     fun getCartTotal(): Double {
-        return cart.map { it.price }.sum()
+        return cart.sumOf { it.price }
     }
 }
