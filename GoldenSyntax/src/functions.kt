ff5732c4474a84b5da6a925aa9ebc1da498a3eb3
nasibfares <nasib.y.fares@gmail.com>
2023-05-05 23:30:12 +0200

    Ein neues Projekt 'Golden Syntax' wurde erstellt.

--- GoldenSyntax/src/functions.kt
+++ GoldenSyntax/src/functions.kt
@@ -68,17 +68,48 @@ Allererstes
 
 fun register() {
     var customer = Customers("", "", "", "", 0, "")
-    var userList=Users("","","","")
-    println("Geben Sie bitte Ihren Vornamen ein")//Vorname des Benutzers
-    val firstName = readln()
-    customer.firstName = firstName
+    var userList = Users("", "", "", "")
+    val maxAttempts = 2
+    var attempts = 0
+    var firstName: String? = ""
+    var lastName: String? = ""
+    while (attempts < maxAttempts) {
+        println("Geben Sie bitte Ihren Vornamen ein")              //Eingabe des Vornamens des Benutzers
+        firstName = readln()
+        //Falls die Eingabe leer oder null oder nicht Buchstaben ist
+        if (firstName.isNullOrEmpty() || !firstName.all { it.isLetter() }) {
+            println("Eingabe darf nicht leer oder null oder Zahlen sein.")
+            attempts++
 
-    println("Geben Sie bitte Ihren Nachnamen ein")//Nachname des Benutzers
-    val lastname = readln()
-    customer.lastName = lastname
+        } else {
+            println("Ihr Vorname lautet: [$firstName]")
+            customer.firstName = firstName
+            break
+        }
+    }
+    if (firstName.isNullOrEmpty() || !firstName.all { it.isLetter() }) {
+        println("Sie haben die maximale Anzahl an Versuchen überschritten.")
+    }
+
+    while (attempts < maxAttempts) {
+        println("Geben Sie bitte Ihren Nachnamen ein")//Nachname des Benutzers
+        lastName = readln()
+        if (lastName.isNullOrEmpty() || !lastName.all { it.isLetter() }) {
+            println("Eingabe darf nicht leer oder null oder Zahlen sein.")
+            attempts++
+        } else {
+            println("Ihr Nachname lautet: [$firstName]")
+            customer.lastName = lastName
+            break
+        }
+    }
+    if (lastName.isNullOrEmpty() || !lastName.all { it.isLetter() }) {
+        println("Sie haben die maximale Anzahl an Versuchen überschritten.")
+    }
+
     println("Geben Sie bitte Ihre E-Mail Adresse ein:")//E-Mail-Adresse des Benutzers
     val email = readln()
-    checkRegisterInfo(email,userList,customer)
+    checkRegisterInfo(email, userList, customer)
     customer.eMail = email
     println("Geben Sie bitte Ihr Alter ein:")//Das Alter des Benutzers
     val age = readln().toIntOrNull() ?: 0
@@ -91,19 +122,22 @@ fun register() {
     }
 }
 
-fun checkRegisterInfo(eMail: String,userList:Users,customer:Customers) { //Überprüfen, ob die E-Mail gültig und nicht schon Verwendet
-    val checkValidity=isValidEmail(eMail)
-    val maxAttempts=2
-    val attempts=0
+fun checkRegisterInfo(
+    eMail: String,
+    userList: Users,
+    customer: Customers
+) { //Überprüfen, ob die E-Mail gültig und nicht schon Verwendet
+    val checkValidity = isValidEmail(eMail)
+
     if (!userList.usersList.any { it.eMail == eMail } && checkValidity) {//E-Mail ist nicht in der Liste und ist gültig.
         userList.usersList.add(customer)
 
     }
-    while(userList.usersList.any { it.eMail == eMail }){
+    while (userList.usersList.any { it.eMail == eMail }) {
         println("Die E-Mail Adresse existiert schon in der Datenbank.!!")
         break
     }
-    while(!checkValidity){
+    while (!checkValidity) {
         println("Die E-Mail Adresse ist nicht gültig.")
         break
     }
