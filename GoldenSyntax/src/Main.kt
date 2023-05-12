2c32f8de0f1ab3515225ee87bc74999cab832b44
nasibfares <nasib.y.fares@gmail.com>
2023-05-12 15:14:52 +0200

    Ein neues Projekt 'Golden Syntax' wurde erstellt.

--- GoldenSyntax/src/Main.kt
+++ GoldenSyntax/src/Main.kt
@@ -4,8 +4,8 @@ import benutzer.Users
 import functios.*
 import produkte.products.finalProductsList
 
-val operator = Users("Nasib", "Fares", "operator@gmail.com", 45, "test", true)
-val normalCustomer = Users("jack", "jack", "customer@gmail.com", 15, "test", false)
+val operator = Users("Nasib", "Fares", "operator@gmail.com", 45, "Test2023", true)
+val normalCustomer = Users("jack", "jack", "customer@gmail.com", 15, "Test2023", false)
 val customer = Customers(
     "", "", "", 13, "", false, finalProductsList,
 
@@ -20,9 +20,7 @@ fun main() {
 
     userList.add(operator)
     userList.add(normalCustomer)
-
     registerLoginLogout()
-
     if (user.adminRight) {
         operatorOption()
 
--- GoldenSyntax/src/produkte/products/Store.kt
+++ GoldenSyntax/src/produkte/products/Store.kt
@@ -88,7 +88,7 @@ var finalProductsList: MutableList<Store> = mutableListOf(
     )
 )
 /*
-Die Klasse 'Products' hat die Eigenschaften "name→ Name des Produkts", "price→ Preis des Produkts",
+Die Klasse 'Store' hat die Eigenschaften "name→ Name des Produkts", "price→ Preis des Produkts",
 "evaluation→ Rezension des Produkts", "category→ Kategorie des Produkts", "subcategory→ Unterkategorie des Produkts"
  */
 open class Store(
